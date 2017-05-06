1f1a18ab5d4e9c0bb91842919671b2dfb84dcd1e
Jevin Studios <jevin.gugelmann@outlook.com>
2017-05-06 20:32:03 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,169 @@
+var canvas = document.getElementById("myCanvas");
+var ctx = canvas.getContext("2d");
+var x = 700;
+var y = 700;
+var chaserX = 50;
+var chaserY = 50;
+var dx = 2;
+var dy = -2;
+var chaserDX = 1.5;
+var chaserDY = -1.5;
+var rightPressed = false;
+var leftPressed = false;
+var upPressed = false;
+var downPressed = false;
+var dPressed = false;
+var aPressed = false;
+var wPressed = false;
+var sPressed = false;
+var ballRadius = 15;
+var chaserRadius = 15;
+var seconds = 0;
+var start = false;
+
+
+
+function drawBall() {
+	ctx.beginPath();
+	ctx.arc(x, y, ballRadius, 0, Math.PI*2, false);
+	ctx.fillStyle = "blue";
+	ctx.fill();
+	ctx.closePath();
+}
+
+function drawChaser() {
+	ctx.beginPath();
+	ctx.arc(chaserX, chaserY, chaserRadius, 0, Math.PI*2, false);
+	ctx.fillStyle = "green";
+	ctx.fill();
+	ctx.closePath();
+}
+
+function drawSeconds() {
+	seconds += 1;
+	document.getElementById("seconds").innerHTML = "Seconds: "+seconds;
+}
+
+function draw() {
+	ctx.clearRect(0, 0, canvas.width, canvas.height);
+	drawBall();
+	drawChaser();
+	if(y + dy > canvas.height-ballRadius || y + dy < ballRadius || x + dx > canvas.width-ballRadius || x + dx < ballRadius) {
+		alert("GAME OVER");
+		document.location.reload();
+	}
+	
+	if(chaserY + chaserDY > canvas.height-chaserRadius || chaserY + chaserDY < chaserRadius || chaserX + chaserDX > canvas.width-chaserRadius || chaserX + chaserDX < chaserRadius) {
+		alert("YOU WIN");
+		document.location.reload();
+	}
+	
+	if((Math.sqrt((Math.pow((chaserX-x), 2)) + (Math.pow((chaserY-y), 2)))) < 30) {
+		alert("YOU COLLIDED");
+		document.location.reload();
+	}
+
+	if(rightPressed) {
+		x += dx;
+	}
+	if(leftPressed) {
+		x -= dx;
+	}
+	if(downPressed) {
+		y -= dy;
+	}
+	if(upPressed) {
+		y += dy;
+	}
+	if(dPressed) {
+		chaserX += chaserDX;
+	}
+	if(aPressed) {
+		chaserX -= chaserDX;
+	}
+	if(sPressed) {
+		chaserY -= chaserDY;
+	}
+	if(wPressed) {
+		chaserY += chaserDY;
+	}
+}
+
+
+document.addEventListener("keydown", keyDownHandler, false);
+document.addEventListener("keyup", keyUpHandler, false);
+
+function keyDownHandler(e) {
+	if(e.keyCode == 39) {
+		rightPressed = true;
+		if(!start) {
+			start = true;
+			setInterval(drawSeconds, 1000);
+		}
+	} else if (e.keyCode == 37) {
+		leftPressed = true;
+		if(!start) {
+			start = true;
+			setInterval(drawSeconds, 1000);
+		}
+	} else if (e.keyCode == 40) {
+		downPressed = true;
+		if(!start) {
+			start = true;
+			setInterval(drawSeconds, 1000);
+		}
+	} else if (e.keyCode == 38) {
+		upPressed = true;
+		if(!start) {
+			start = true;
+			setInterval(drawSeconds, 1000);
+		}
+	} else if (e.keyCode == 65) {
+		aPressed = true;
+		if(!start) {
+			start = true;
+			setInterval(drawSeconds, 1000);
+		}
+	} else if (e.keyCode == 68) {
+		dPressed = true;
+		if(!start) {
+			start = true;
+			setInterval(drawSeconds, 1000);
+		}
+	} else if (e.keyCode == 83) {
+		sPressed = true;
+		if(!start) {
+			start = true;
+			setInterval(drawSeconds, 1000);
+		}
+	} else if (e.keyCode == 87) {
+		wPressed = true;
+		if(!start) {
+			start = true;
+			setInterval(drawSeconds, 1000);
+		}
+	}
+}
+
+function keyUpHandler(e) {
+	if(e.keyCode == 39) {
+		rightPressed = false;
+	} else if (e.keyCode == 37) {
+		leftPressed = false;
+	} else if (e.keyCode == 40) {
+		downPressed = false;
+	} else if (e.keyCode == 38) {
+		upPressed = false;
+	} else if (e.keyCode == 65) {
+		aPressed = false;
+	} else if (e.keyCode == 68) {
+		dPressed = false;
+	} else if (e.keyCode == 83) {
+		sPressed = false;
+	} else if (e.keyCode == 87) {
+		wPressed = false;
+	}
+	
+}
+
+setInterval(draw, 10);
